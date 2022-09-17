27a06a79545d7d88ce0f2388b108d37b0ea852b1
Ethel <34180184+lyq323@users.noreply.github.com>
2022-09-17 22:05:51 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 // vitepress/config.js
 module.exports = {
     title: "我的博客",// 网站标题
-    description: '我的vitepress博客.', //网站描述
+    description: '保持清醒.', //网站描述
     base: '/docs/', //  部署时的路径 默认 /  可以使用二级地址 /base/
     lang: 'en-US' //语言
 }
