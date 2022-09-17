1d15a1b1b3fcf45ed460fb94fe2280e662570db5
lyq323 <1129829029@qq.com>
2022-09-17 19:24:10 +0800

    update

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+// vitepress/config.js
+module.exports = {
+    title: "我的博客",// 网站标题
+    description: '我的vitepress博客.', //网站描述
+    base: '/', //  部署时的路径 默认 /  可以使用二级地址 /base/
+    lang: 'en-US' //语言
+}
