aaec7fca6e24fedaa62d23e4e7f7e3cbbcb5c610
Ethel <34180184+lyq323@users.noreply.github.com>
2022-09-17 22:09:23 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,6 @@
 module.exports = {
     title: "我的博客",// 网站标题
     description: '保持清醒.', //网站描述
-    base: '/docs/', //  部署时的路径 默认 /  可以使用二级地址 /base/
+    base: '/lyq323/', //  部署时的路径 默认 /  可以使用二级地址 /base/
     lang: 'en-US' //语言
 }
